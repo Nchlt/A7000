1170fb2a456d72141473f3e1759fa417ef9bcac2
Malik Kazi Aoual <mkaziao@a-018818.ups.u-psud.fr>
2017-12-22 19:49:55 +0100

    avant depart

--- SourcetoTyped.ml
+++ SourcetoTyped.ml
@@ -8,29 +8,24 @@ module Symb_Tbl = S.Symb_Tbl
 (* Rapports d'erreurs *)
 exception Type_error of S.typ * S.typ
     
-(* comparetype: typ -> typ -> unit
-   Lève une exception si les types diffèrent. *)
+(*Fonction récupérée de SourceTypChecker*)
 let comparetype t1 t2 =
   if t1 <> t2
   then raise (Type_error(t1, t2))
-    
-(* type_literal: literal -> typ *)
+
+(*Des fonctions qui récupèrent les types pour pouvoir construire les annotated elements*)
+
 let type_literal : S.literal -> S.typ = function
   | Int _  -> TypInteger
   | Bool _ -> TypBoolean
      
-(* [type_binop] renvoie le type des opérandes et le type du résultat
-   d'un opérateur binaire. *)
-(* type_binop: binop -> typ * typ *)
+
 let type_binop : S.binop -> S.typ * S.typ = function
   | Add | Sub | Mult     -> TypInteger, TypInteger
   | Eq  | Neq | Lt  | Le -> TypInteger, TypBoolean
   | And | Or             -> TypBoolean, TypBoolean
     
-(* [type_expression] et les fonctions associées prennent en paramètre
-   le programme lui-même (pour connaître les types des fonctions) et
-   la table de symboles locale (pour connaître les types des variables).
-   Leur résultat est de type [typ]. *)
+
 let rec type_expression prog symb_tbl = function
   | S.Literal(lit)  -> type_literal lit
   | S.Location(loc) -> type_location prog symb_tbl loc
@@ -41,26 +36,25 @@ let rec type_expression prog symb_tbl = function
      ty_r
   | S.FunCall(c) ->
      (match type_call prog symb_tbl c with
-     | None -> failwith "Non-void function expected"
+     | None -> failwith "Type fail"
      | Some t -> t)
   | S.NewArray(e, t) ->
      comparetype TypInteger (type_expression prog symb_tbl e);
     TypArray(t)
+
       
 and type_location prog symb_tbl = function
   | S.Identifier(id)  -> (Symb_Tbl.find id symb_tbl).S.typ
   | S.ArrayAccess(aa) -> type_a_access prog symb_tbl aa
      
-(* Un accès bien formé demande que [e1] désigne un tableau et [e2] un entier *)
+
 and type_a_access prog symb_tbl (e1, e2) =
   comparetype TypInteger (type_expression prog symb_tbl e2);
   match type_expression prog symb_tbl e1 with
   | TypArray(t) -> t
   | _ -> failwith "Array type expected"
        
-(* Un appel bien formé demande que chaque paramètre effectif ait un type 
-   compatible avec le paramètre formel correspondant *)
-(* Contrairement aux précédentes, [type_call] renvoie un résultat [typ option] *)
+
 and type_call prog symb_tbl (fid, args) =
   let (f : S.function_info) = List.assoc fid prog in
   let expected_types = List.map snd f.formals in
@@ -81,8 +75,6 @@ let annote_function p f =
     | If(e, b1, b2) -> T.If(annote_expression e, annote_block b1, annote_block b2)
     | Print(e)      -> T.Print(annote_expression e)
     | ProcCall(c)   -> T.ProcCall(annote_call c)
-    (* | Throw         -> failwith "Pas encore implémenté" *)
-    (* | Try(b1, b2)   -> failwith "Pas encore implémenté" *)
       
   and annote_expression expr =
     let annot = type_expression p symb_tbl expr in
